c8ce22ea198ed2ebde15fda3661b1871543ddf0c
weeklyalgo <weeklyalgo@163.com>
2017-08-01 23:08:40 +0800

    Site updated: 2017-08-01 23:08:39

---
+++ js/src/bootstrap.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  Even.fancybox.register();
+  Even.mobileNavbar.register();
+  Even.search.register();
+  Even.postToc.register();
+  Even.backToTop.register();
+})
